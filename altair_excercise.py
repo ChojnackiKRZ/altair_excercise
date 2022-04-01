2462155efa87eb164cdaa76ff8a9a5b631d65c51
Krzysztof Chojnacki <krzysztof.michal.chojnacki@gmail.com>
2022-04-01 21:53:42 +0200

    Update altair_excercise.py

--- altair_excercise.py
+++ altair_excercise.py
@@ -1,23 +1,35 @@
 import altair as alt
 import seaborn as sns
+from os import chdir
+
+path = (
+    r"C:\Users\krzys\Desktop\data science\IV semestr\machine_learning\altair_excercise"
+)
+
+chdir(path)
 
 source = sns.load_dataset("penguins")
 
-base = alt.Chart(source).mark_point(size=30).encode(
-    alt.X('bill_length_mm', scale = alt.Scale(zero=False)),
-    alt.Y('flipper_length_mm', scale = alt.Scale(zero=False)),
-    color='species',
-    tooltip= 'species')
+base = (
+    alt.Chart(source)
+    .mark_point(size=30)
+    .encode(
+        alt.X("bill_length_mm", scale=alt.Scale(zero=False)),
+        alt.Y("flipper_length_mm", scale=alt.Scale(zero=False)),
+        color="species",
+        tooltip="species",
+    )
+)
 
 degree_list = [1, 3, 5]
 
 polynomial_fit = [
     base.transform_regression(
-        "bill_length_mm", 
-        "flipper_length_mm", 
-        method="poly", 
-        order=order, 
-        as_=["bill_length_mm", str(order)]
+        "bill_length_mm",
+        "flipper_length_mm",
+        method="poly",
+        order=order,
+        as_=["bill_length_mm", str(order)],
     )
     .mark_line()
     .transform_fold([str(order)], as_=["degree", "flipper_length_mm"])
@@ -26,33 +38,4 @@ polynomial_fit = [
 ]
 
 final = alt.layer(base.interactive(), *polynomial_fit)
-
-#%%
-import numpy as np
-import pandas as pd
-import altair as alt
-
-# Generate some random data
-rng = np.random.RandomState(1)
-x = rng.rand(40) ** 2
-y = 10 - 1.0 / (x + 0.1) + rng.randn(40)
-source = pd.DataFrame({"x": x, "y": y})
-
-# Define the degree of the polynomial fits
-degree_list = [1, 3, 5]
-
-base = alt.Chart(source).mark_circle(color="black").encode(
-        alt.X("x"), alt.Y("y")
-)
-
-polynomial_fit = [
-    base.transform_regression(
-        "x", "y", method="poly", order=order, as_=["x", str(order)]
-    )
-    .mark_line()
-    .transform_fold([str(order)], as_=["degree", "y"])
-    .encode(alt.Color("degree:N"), legend=alt.Legend(values=['degree_list']))
-    for order in degree_list
-]
-
-alt.layer(base, *polynomial_fit)
+final.save("Chojnacki_Krzysztof_altair.html")
